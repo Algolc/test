c9017fc060aab1d1c0830ea061075ab7cf2200a0
algolc <algolc@>
2020-06-12 13:07:10 +0800

    提交代码

---
+++ helloWorld.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import sys
+
+print("Hello World!")
+
+if True:
+    print("True")
+
+# 这是一行注释
+'''
+else:
+    print('False')
+'''
+
+#  str = input("\n\n输入回车后退出")
+
+x = 'runoob'
+sys.stdout.write(x + '\n')
+
+y = 123.0
+isinstance(y, int)
+
+print(y)
+
